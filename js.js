fc44c2a17c79971be557d15a5d57ca1ed4d1fa79
ziyalogy <ziyalogy@gmail.com>
2021-10-26 10:52:16 +0300

    Swith a light bulb on and off with VueJS

--- js.js
+++ js.js
@@ -5,14 +5,21 @@ const app = Vue.createApp({
             image: './images/lightOff.png',
             switchOn: false,
             states: [
-              { id: 1, name:'On', color: '#ffcc00', image: './images/lightOn.png' },
-              { id: 2, name:'Off', color: 'black', image: './images/lightOff.png' },
+              { id: 1, name:'On', color: '#2abb9b', image: './images/lightOn.png' },
+              { id: 2, name:'Off', color: '#96281b', image: './images/lightOff.png' },
+            ],
+            bodyState: [
+                { id: 1, name:'On', bgColor: '#ffcc00' },
+              { id: 2, name:'Off', bgColor: '#bfbfbf'}, 
             ]
         }
     },
     methods: {
         updateImage(stateImage) {
             this.image = stateImage
+        },
+        changeBodyBg(bodyBG) {
+            this.bodyState = bodyBG
         }
     }
     })
